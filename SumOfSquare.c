cf41595358f3800c1ca5c0b029e75bd70159fa49
DeepaH29 <deepa.hanamapure@walchandsangli.ac.in>
2025-02-23 22:03:39 +0530

    Update SumOfSquare.c

--- SumOfSquare.c
+++ SumOfSquare.c
@@ -2,27 +2,23 @@
 #include <omp.h>
 
 int main() {
-    int total_threads, sum_of_squares = 0;
+    int sum = 0;
 
     #pragma omp parallel
     {
         int thread_id = omp_get_thread_num();
         int square = thread_id * thread_id;
 
-        // Print the square of the thread ID
+        // Each thread prints its square
         printf("Thread %d: Square = %d\n", thread_id, square);
 
-        // Use reduction to compute sum of squares safely in parallel
+        // Use atomic to safely update the sum
         #pragma omp atomic
-        sum_of_squares += square;
+        sum += square;
     }
 
-    // Print the final sum of squares (only once)
-    #pragma omp barrier
-    #pragma omp master
-    {
-        printf("Sum of squares of thread IDs = %d\n", sum_of_squares);
-    }
+    // Print final sum outside the parallel region
+    printf("Sum of squares of thread IDs: %d\n", sum);
 
     return 0;
 }
